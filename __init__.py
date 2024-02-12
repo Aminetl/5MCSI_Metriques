ddbd45675ebedfa676539be48e3163f6bfa528fe
Aminetl <159786650+Aminetl@users.noreply.github.com>
2024-02-12 15:29:37 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,9 +8,5 @@ import sqlite3
 app = Flask(__name__)                                                                                                                  
 def hello_world():
     return render_template('hello.html') #commentairess
-  @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"                                                                                                                                    
-@app.route('/')
 if __name__ == "__main__":
   app.run(debug=True)
